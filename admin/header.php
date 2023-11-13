dbf74056cc53b215f9de97c1eb31bd41656cd5da
Duy-Loi <leduyloi16504@gmail.com>
2023-11-13 12:51:53 +0700

    test

--- admin/header.php
+++ admin/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Danh sách loại hàng</title>
+    <title>Dự án 1</title>
     <link rel="stylesheet" href="../css/admin.css">
 
 </head>
